e9c51c0fabef6dfaab8068d15d2040472d2838cf
Ronald-Trevor <101838410+Ronald-Trevor@users.noreply.github.com>
2023-08-23 23:23:30 +0300

    remove node
    defining a function that is used to delete a node from the list

--- circular_linked_list.py
+++ circular_linked_list.py
@@ -40,9 +40,34 @@ class CircularLinkedList:
 			cur.next = new_node
 		self.head = new_node
 
+	def remove(self,key):
+		if self.head:
+			if self.head.data == key:
+				cur = self.head
+				while cur.next != self.head :
+					cur = cur.next 
+				if self.head == self.head.next :
+					self.head = None 
+				else:
+					cur.next = self.head.next
+					self.head = self.head.next
+			else:
+				cur = self.head 
+				prev = None 
+				while cur.next != self.head:
+					prev = cur
+					cur = cur.next 
+					if cur.data == key:
+						prev.next = cur.next 
+						cur = cur.next
+
+
 list1 = CircularLinkedList()
 list1.append("C")
 list1.append("D")
 list1.preppend("B")
 list1.preppend("A")
+list1.print_list()
+print("\n")
+list1.remove("C")
 list1.print_list()
